d9e8e93789d676be02bccc7a36619fbabb14e084
Justin Luque <justin.luque@gmail.com>
2024-09-24 22:14:59 -0400

    feat: create main loop skeleton

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,32 @@
+#include <cctype>
 #include <iostream>
+
 #include "gradebook.h"
 #include "student.h"
 
-int main()
-{
-  std::cout << "Hello world!";
-  return 0;
+void flushCin() {
+  std::cin.clear();
+  std::cin.ignore(256, '\n');
+}
+
+int main() {
+  std::cout << "Welcome to Consumer Softproducts interactive C++ gradebook!\n";
+  while (true) {
+    // TODO: print menu from class
+
+    std::string input;
+    std::cout << "Please enter an action (q to exit).\n";
+    std::cin >> input;
+
+    switch (tolower(input.at(0))) {
+      case 'q':
+        // TODO: save grades?
+        return 0;
+        break;
+
+      // TODO: detail out interface after completing class implementations
+      default:
+        break;
+    }
+  }
 }
