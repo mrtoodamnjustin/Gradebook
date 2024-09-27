b7036129d946397320f9ae9ef80ef3672de498c9
Justin Luque <justin.luque@gmail.com>
2024-09-26 20:18:15 -0400

    ref: clean up `Gradebook` implementation

--- src/gradebook.cpp
+++ src/gradebook.cpp
@@ -10,15 +10,7 @@ std::vector<Student> Gradebook::getStudents() { return this->students; }
 
 std::vector<Course> Gradebook::getCourses(int studentUID) {
   std::vector<Course> studentsCourses = {};
-  Student* student = nullptr;
-
-  // Find the student object, if it exists
-  for (size_t i = 0; i < this->students.size(); i++) {
-    if (this->students[i].getUID() == studentUID) {
-      student = &students[i];
-      break;
-    }
-  }
+  Student* student = getStudent(studentUID);
 
   if (student == nullptr) return {};
 
@@ -26,12 +18,9 @@ std::vector<Course> Gradebook::getCourses(int studentUID) {
 
   // Gather all the course objects
   for (size_t i = 0; i < courseCRNs.size(); i++) {
-    for (size_t j = 0; j < this->courses.size(); j++) {
-      if (this->courses[j].getCRN() == courseCRNs[i]) {
-        studentsCourses.push_back(this->courses[j]);
-        break;
-      }
-    }
+    Course* course = getCourse(courseCRNs[i]);
+
+    if (course != nullptr) studentsCourses.push_back(*course);
   }
 
   return studentsCourses;
@@ -39,45 +28,31 @@ std::vector<Course> Gradebook::getCourses(int studentUID) {
 
 std::vector<Student> Gradebook::getStudents(int courseCRN) {
   std::vector<Student> studentsInCourse = {};
-  Course* course = nullptr;
 
-  for (size_t i = 0; i < this->courses.size(); i++) {
-    if (this->courses[i].getCRN() == courseCRN) {
-      course = &this->courses[i];
-      break;
-    }
-  }
+  Course* course = getCourse(courseCRN);
 
-  if (course == nullptr) {
-    return {};
-  }
+  if (course == nullptr) return {};
 
   std::vector<int> studentUIDs = course->getStudents();
 
   for (size_t i = 0; i < studentsInCourse.size(); i++) {
-    for (size_t j = 0; j < this->students.size(); j++) {
-      if (studentUIDs[i] == this->students[j].getUID())
-        studentsInCourse.push_back(this->students[j]);
-    }
+    Student* student = getStudent(studentUIDs[i]);
+
+    if (student != nullptr) studentsInCourse.push_back(*student);
   }
 
   return studentsInCourse;
 }
 
 float Gradebook::getGrade(int studentUID, int courseCRN) {
-  Course* course = nullptr;
-
-  for (size_t i = 0; i < this->courses.size(); i++) {
-    if (this->courses[i].getCRN() == courseCRN) course = &this->courses[i];
-    break;
-  }
+  Course* course = this->getCourse(courseCRN);
 
   if (course == nullptr) return -1;
 
   float sum = 0, totalGrades = 0;
   auto assignments = course->getAssignments();
-  for (auto i = assignments.begin(); i != assignments.end(); i++) {
-    auto& studentGrades = i->second;
+  for (auto it = assignments.begin(); it != assignments.end(); it++) {
+    auto& studentGrades = it->second;
     auto studentIt = studentGrades.find(studentUID);
 
     if (studentIt != studentGrades.end()) {
