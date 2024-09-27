b7b7d8c126393fbf5a19317d0503b517ee53d4b5
mrtoodamnjustin <justin.luque@gmail.com>
2024-09-27 15:51:18 -0400

    feat: course class now properly handles new and removed student's grades in `addStudent()` and `removeStudent()` methods

--- src/course.cpp
+++ src/course.cpp
@@ -41,7 +41,18 @@ void Course::deleteAssignment(std::string assignment) {
 
 // Students
 void Course::addStudent(int studentUID) {
+  auto it = std::find(this->students.begin(), this->students.end(), studentUID);
+
+  // If the student already is in the class, return
+  if (it != this->students.end()) return;
+
   this->students.push_back(studentUID);
+
+  // Set student's assignments in the class to ungraded
+  for (auto it = this->assignments.begin(); it != this->assignments.end();
+       it++) {
+    it->second[studentUID] = Ungraded;
+  }
 }
 
 void Course::removeStudent(int studentUID) {
@@ -49,6 +60,12 @@ void Course::removeStudent(int studentUID) {
 
   // If the student is already not in the class, return
   if (it == this->students.end()) return;
+
+  // Erase student's grades from the course
+  for (auto it = this->assignments.begin(); it != this->assignments.end();
+       it++) {
+    it->second.erase(studentUID);
+  }
 
   this->students.erase(it);
 }
